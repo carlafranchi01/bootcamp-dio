26f59199cd5c025e2f76c9d7f50083fccfdf4d67
Carla <100912030+carlafranchi01@users.noreply.github.com>
2022-07-22 13:51:34 -0300

    Add files via upload

---
+++ Devs.java
@@ -0,0 +1,11 @@
+public class Devs {
+
+    private String nomeDoDev;
+
+    public String getNomeDoDev() {
+        return nomeDoDev;
+    }
+    public void setNomeDoDev(String nomeDoDev) {
+        this.nomeDoDev = nomeDoDev;
+    }
+}
