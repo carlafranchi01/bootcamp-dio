985cb37f074f866f53e3cfa312fcf118bd02a4d1
Carla <100912030+carlafranchi01@users.noreply.github.com>
2022-07-22 13:51:10 -0300

    Add files via upload

---
+++ Cursos.java
@@ -0,0 +1,11 @@
+public class Cursos {
+
+    private String nomeDoCurso;
+
+    public String getNomeDoCurso() {
+        return nomeDoCurso;
+    }
+    public void setNomeDoCurso(String nomeDoCurso) {
+        this.nomeDoCurso = nomeDoCurso;
+    }
+}
