1e99e6e65079d9a1edf0a20db31b0f93c56c5a79
Carla <100912030+carlafranchi01@users.noreply.github.com>
2022-07-22 13:51:56 -0300

    Add files via upload

---
+++ Mentorias.java
@@ -0,0 +1,13 @@
+
+
+public class Mentorias {
+
+    private String nomeDaMentoria;
+
+    public String getNomeDaMentoria() {
+        return nomeDaMentoria;
+    }
+    public void setNomeDaMentoria(String nomeDaMentoria) {
+        this.nomeDaMentoria = nomeDaMentoria;
+    }
+}
