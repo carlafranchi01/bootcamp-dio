a35017991cfd2bf1264d8e159a83a2f94ebcb0d4
Carla <100912030+carlafranchi01@users.noreply.github.com>
2022-07-22 13:50:36 -0300

    Add files via upload

---
+++ Bootcamp.java
@@ -0,0 +1,13 @@
+public class Bootcamp {
+    public static void main(String[] args){
+
+        //instanciacao do objeto bootcamp e inicializacao do metodo
+
+        Bootcamp bootcamp = new Bootcamp();
+
+        bootcamp.setNomeDoCurso("Santander Code Girls");
+        bootcamp.setNomeDoDev("Camilla Cavalcante");
+        bootcamp.setNomeDaMentoria("Introducao a POO em Java");
+
+     }
+}
